943aa1f334d5fcbaf6088d54e1556144796fef1d
pavankumar <kpkumbar99@gmail.com>
2023-08-31 18:49:28 +0200

    done login page

--- Dj_CRM/website/views.py
+++ Dj_CRM/website/views.py
@@ -1,5 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+
+#username: admin
+#password: pavan123
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    # check if they login
+    print(request.method)
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        #authemticate
+        user = authenticate(request, username=username, password=password)
+        print("user", user)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have successfully logged in")
+            return redirect('home')
+        else:
+            messages.success(request, "Error logging in Please login again")
+            return redirect('home')
+
+    else:
+        return render(request, 'home.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have successfully logged out")
+    return redirect('home')
+
